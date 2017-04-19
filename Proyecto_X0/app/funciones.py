50e7fe95b736f3bba6baa6d3ed173f30fff70fbc
MSI\samle <samleon8@gmail.com>
2017-04-19 13:50:36 -0500

    Se añadieron comentarios para la documentacion y se añarieron aspectos esteticos

--- Proyecto_X0/app/funciones.py
+++ Proyecto_X0/app/funciones.py
@@ -1,9 +1,19 @@
+'''
+En este modulo estan todas las funciones complejas necesarias para que funcione el programa
+'''
 import random
-from flask import Flask,render_template
 
 
 def evalua_ganardor(p):
-    ganador = "no"    # determina si alguno de los jugadores a ganado
+    '''
+    Esta funcion se usa para ver cuando alguno de los jugadores gana
+    :param p: Es el array que contiene la informacion de que posiciones se jugaron
+    :type p: str
+    
+    :return: retorna quien gano ('X' o '0') o 'no' cuando nadie a ganado 
+    :rtype: str
+    '''
+    ganador = "no"    # Estas son todas las posibles formas de ganar en el juego
     if p[1] == p[2] == p[3]:
         ganador = p[1]
     if p[4] == p[5] == p[6]:
@@ -22,20 +32,21 @@ def evalua_ganardor(p):
         ganador = p[1]
     return ganador
 
-def leer_posicion(a):
-    #pos = random.randint(1, 9)
-    #pos = int(input("posicion: "))
-    # aqui hay que hacer la conecion con la parte grafica del juego
-    # al hacer clic en la posicion que se desea jugar debe devolver un valor entre 1-9 segun la posicion
-    pos = a
-    return pos
 
 def IA(p):
+    '''
+    Esta funcion genera las jugadas que va a hacer la computadora
+    
+    :param p: Es el array que contiene la informacion de que posiciones se jugaron
+    :type p: str
+    
+    :return: regresa la posicion que va a jugar la computadora
+    :rtype: int
+    '''
     pos = 0
-
     i = 1
     flag = True
-    while flag:
+    while flag:     # primero evalua si la IA ganaria al jugar en alguna posicion, de ser asi juega esa para ganar
         if p[i] == i:
             p[i] = '0'
             if evalua_ganardor(p) == '0':
@@ -46,7 +57,7 @@ def IA(p):
             flag = False
         i += 1
 
-    if pos == 0:
+    if pos == 0:        # si no obtine una posicion que jugar aun, evalua si el jugador ganaria al jugar una posicion, de ser asi juega esa para no perder
         i = 1
         flag = True
         while flag:
@@ -60,7 +71,7 @@ def IA(p):
                 flag = False
             i +=1
 
-    if pos == 0:
+    if pos == 0:        # si aun no obtiene una posicion que jugar escoje una al azar entre las disponibles
         flag = True
         while flag:
             i = random.randint(1, 9)
--- Proyecto_X0/app/main.py
+++ Proyecto_X0/app/main.py
@@ -1,74 +1,59 @@
-from app.funciones import evalua_ganardor, leer_posicion, IA
+from app.funciones import evalua_ganardor, IA
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+# contadores usados durante la ejecucion del programa. Se inicializan en 0 al inicio
 contador = 0
-p = ['a']
+xgana = 0
+ogana = 0
+empate = 0
+p = ['a'] # Array que contiene los datos de las posiciones que se juegan. Por comodidas mias la posicion 0 del array no se usa por eso la 'a'
 for i in range(1, 10):  #inicializar con datos diferentes
     p.append(i)
 
 
 @app.route('/')
 def inicio():
+    '''
+    Funcion inicial. Reinicia los datos de las posiciones para un juego nuevo.
+    
+    :return: Enseña el 'index.html' en blanco y le envia los datos que necesita para funcionar
+    '''
     for i in range(1,10):
         p[i]=i
     global contador
     contador=0
-    return render_template('index.html', p1=p[1], p2=p[2], p3=p[3], p4=p[4], p5=p[5], p6=p[6], p7=p[7], p8=p[8], p9=p[9])
+    return render_template('index.html', p1=p[1], p2=p[2], p3=p[3], p4=p[4], p5=p[5], p6=p[6], p7=p[7], p8=p[8], p9=p[9], gx=xgana, g0=ogana, em=empate)
 
 
 @app.route('/jugada', methods=['POST'])
 def jugada():
+    '''
+    Aqui sucede todo lo importante. Se llevan a cabo las jugadas
+    
+    :return: Enseña 'resultado.html' cuando termina el juego o 'index.html' para seguir jugando
+    '''
     if request.method == 'POST':
+        global contador, xgana, ogana, empate
         pos = int(request.form.get("posicion"))
-        p[leer_posicion(pos)] = 'x'
-        global contador
+        p[pos] = 'x'
         contador+=1
         ganador = evalua_ganardor(p)
         if ganador == 'x':
-            return render_template('resultado.html', gana=ganador, p1=p[1], p2=p[2], p3=p[3], p4=p[4], p5=p[5], p6=p[6], p7=p[7], p8=p[8], p9=p[9])
+            xgana+=1
+            return render_template('resultado.html', gana=ganador, p1=p[1], p2=p[2], p3=p[3], p4=p[4], p5=p[5], p6=p[6], p7=p[7], p8=p[8], p9=p[9], gx=xgana, g0=ogana, em=empate)
         if contador ==5:
-            return render_template('resultado.html', gana="NADIE", p1=p[1], p2=p[2], p3=p[3], p4=p[4], p5=p[5], p6=p[6], p7=p[7], p8=p[8], p9=p[9])
+            empate+=1
+            return render_template('resultado.html', gana="NADIE", p1=p[1], p2=p[2], p3=p[3], p4=p[4], p5=p[5], p6=p[6], p7=p[7], p8=p[8], p9=p[9], gx=xgana, g0=ogana, em=empate)
         else:
             a = IA(p)
             p[a] = '0'
             ganador = evalua_ganardor(p)
             if ganador == '0':
-                return render_template('resultado.html', gana=ganador, p1=p[1], p2=p[2], p3=p[3], p4=p[4], p5=p[5], p6=p[6], p7=p[7], p8=p[8], p9=p[9])
-        return render_template('index.html', p1=p[1], p2=p[2], p3=p[3], p4=p[4], p5=p[5], p6=p[6], p7=p[7], p8=p[8], p9=p[9])
-
+                ogana+=1
+                return render_template('resultado.html', gana=ganador, p1=p[1], p2=p[2], p3=p[3], p4=p[4], p5=p[5], p6=p[6], p7=p[7], p8=p[8], p9=p[9], gx=xgana, g0=ogana, em=empate)
+        return render_template('index.html', p1=p[1], p2=p[2], p3=p[3], p4=p[4], p5=p[5], p6=p[6], p7=p[7], p8=p[8], p9=p[9], gx=xgana, g0=ogana, em=empate)
 
 
 if __name__ == "__main__":
     app.run()
-
-
-'''  ESTE FUE EL PRIMER PROTOTIPO ANTES DE AÑADIR EL FLASK EN LA VERSION FINAL ESTAS LINEAS SERAN ELIMINADAS
-    flag = True
-    i=1
-    while flag:
-        if i%2 == 0:   #decide a quien le toca jugar
-            print("\nturno de 0")
-            a = IA(p)
-            p[a] = '0'
-            print(" IA juega en la posicion: "+ str(a))
-        else:
-            print("\nturno de X")
-            p[leer_posicion()] = 'x'
-        ganador = evalua_ganardor(p)
-        if ganador == "x" or ganador == "0":
-            print("Ganador: " + str(ganador))
-            flag = False
-        else:
-            if i == 9:
-                print("Empate")
-            else:
-                print("Siguiente turno")
-
-        if i == 9:  #rompe el ciclo si luego de 9 turnos no hay ganador
-           flag = False
-        i +=1
-
-    print("\n Fin del juego")
-'''
